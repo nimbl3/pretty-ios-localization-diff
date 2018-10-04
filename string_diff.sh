2a888634b97a992998de6fa4948e5404cfb44399
Jason Nam <contact@jasonnam.com>
2018-10-04 11:30:00 +0700

    Check lowercase file names

--- string_diff.sh
+++ string_diff.sh
@@ -5,12 +5,19 @@ read old_tag
 printf "new tag: "
 read new_tag
 
-locales=('en' 'id' 'ja' 'ko' 'th' 'zh-Hans' 'zh-Hant')
+locales=('en' 'id' 'ja' 'th' 'zh-Hant')
+lowercase_locales=('ko' 'zh-Hans')
 
 for locale in "${locales[@]}"
 do
-   git diff $old_tag --color  -- "Tune/Resources/Strings/$locale.lproj/Localizable.strings" | ansi-to-html -n > "Localization-Diff/$locale.strings.html"
-   git diff $new_tag --color -- "Tune/Resources/Strings/$locale.lproj/Localizable.stringsdict" | ansi-to-html -n > "Localization-Diff/$locale.stringsdict.html"
+   git diff $old_tag $new_tag --color -- "Tune/Resources/Strings/$locale.lproj/Localizable.strings" | ansi-to-html -n > "Localization-Diff/$locale.strings.html"
+   git diff $old_tag $new_tag --color -- "Tune/Resources/Strings/$locale.lproj/Localizable.stringsdict" | ansi-to-html -n > "Localization-Diff/$locale.stringsdict.html"
+done
+
+for locale in "${lowercase_locales[@]}"
+do
+   git diff $old_tag $new_tag --color -- "Tune/Resources/Strings/$locale.lproj/localizable.strings" | ansi-to-html -n > "Localization-Diff/$locale.strings.html"
+   git diff $old_tag $new_tag --color -- "Tune/Resources/Strings/$locale.lproj/Localizable.stringsdict" | ansi-to-html -n > "Localization-Diff/$locale.stringsdict.html"
 done
 
 echo "Done! Check 'Localization-Diff/'"
